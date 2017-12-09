ae647a141dc0bbb50fc331115158632ab13e3abe
Sachin Saini <sachin17897@gmail.com>
2017-12-09 17:17:22 +0530

    first commit

---
+++ js/accordion.js
@@ -0,0 +1,9 @@
+
+$(function(){
+    $('.first_head').click(function(){
+      $('.first_para').toggleClass('open');
+    });
+    $('.second_head').click(function(){
+      $('.second_para').toggleClass('open');
+    });
+  });
